ef96bdbb2ce5ce11787e4ee1decedcc86ab63ce1
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-06-06 14:32:47 -0400

    bound

---
+++ index.js
@@ -0,0 +1,19 @@
+function countdown(callback) {
+  window.setTimeout(callback, 2000)
+}
+
+function createMultiplier(multiplierValue) {
+  return (number) => {
+    return multiplierValue * number
+  }
+}
+
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
+
+function multiplier(multiplierValue, number) {
+  return multiplierValue * number
+}
+
+var doublerWithBind = multiplier.bind(null, 2)
+var triplerWithBind = multiplier.bind(null, 3)
